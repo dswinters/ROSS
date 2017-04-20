b2df6302f3bcbf8c020037886cd939d6bd73e787
Dylan Winters <dylan@dw-mbp.local>
2017-04-20 09:55:03 -0700

    wrong kayak!

--- Code/petersburg_2017_apr/petersburg_2017_apr_casey.m
+++ Code/petersburg_2017_apr/petersburg_2017_apr_casey.m
@@ -8,7 +8,7 @@ casey0.heading_offset = 135;
 dep = 1;
 casey(dep).name       = 'CASEY_test1';
 casey(dep).tlim       = datenum([;]);
-casey(dep).files.adcp = {'ADCP_raw_20170418230229.bin'};
-casey(dep).files.gps  = {'GPS_20170418230229.log'};
+casey(dep).files.adcp = {};
+casey(dep).files.gps  = {};
 %--------------------------------------------------------%
 casey = ross_fill_defaults(casey,casey0);
--- Code/petersburg_2017_apr/petersburg_2017_apr_rosie.m
+++ Code/petersburg_2017_apr/petersburg_2017_apr_rosie.m
@@ -7,8 +7,8 @@ rosie0.heading_offset = 135;
 %--------------------------------------------------------%
 dep = 1;
 rosie(dep).name       = 'ROSIE_test1';
-rosie(dep).tlim       = datenum([;]);
-rosie(dep).files.adcp = {};
-rosie(dep).files.gps  = {};
+rosie(dep).tlim       = datenum([-inf inf]);
+rosie(dep).files.adcp = {'ADCP_raw_20170418230229.bin'};
+rosie(dep).files.gps  = {'GPS_20170418230229.log'};
 %--------------------------------------------------------%
 rosie = ross_fill_defaults(rosie,rosie0);

ec6c3035ae3cfed8c9897f39a002041fdfa2c875
Dylan Winters <dylan@dw-mbp.local>
2017-04-21 18:03:17 -0700

    -

--- Code/petersburg_2017_apr/petersburg_2017_apr_deployments.m
+++ Code/petersburg_2017_apr/petersburg_2017_apr_deployments.m
@@ -53,8 +53,5 @@ swankie = ross_fill_defaults(swankie,defaults);
 %========================================================
 % Combine deployment structures into final cell array
 %========================================================
-% deployments = {rosie, swankie};
-% master.kayaks = {'Rosie','Swankie'};
-deployments = {rosie};
-master.kayaks = {'Rosie'};
-
+deployments = {rosie, swankie};
+master.kayaks = {'Rosie','Swankie'};

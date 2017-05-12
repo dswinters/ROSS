b5efb97124eda8ee98dc99a829ddd3ba6d4fe390
Dylan Winters <dylan@dw-mbp.local>
2017-05-12 13:43:45 -0800

    initial version

---
+++ Code/ross_deployment_sections.m
@@ -0,0 +1,26 @@
+%% ross_deployment_sections.m
+% Usage: ross = ross_deployment_sections(ross,dep,tlims)
+% Description: Create deployment structures specifying sections
+%              by copying settings from ross(dep) and using the
+%              time limits specified by tlims.
+% Inputs: ross - ross deployment structure
+%          dep - deployment number from which to copy settings
+%        tlims - datenum vector of section time limits
+%      namefmt - format string to name sections
+% Outputs: ross - deployment structure with sections appended
+% 
+% Author: Dylan Winters
+% Created: 2017-05-12
+function [ross dep] = ross_deployment_sections(ross,dep,tlims,namefmt)
+
+tlims = reshape(tlims,2,[])';
+d0 = ross(dep);
+d0.proc.adcp_raw2mat = false;
+d0.proc.gps_raw2mat = false;
+
+for i = 1:length(tlims)
+    dep = dep+1;
+    ross(dep) = d0;
+    ross(dep).tlim = tlims(i,:);
+    ross(dep).name = sprintf(namefmt,i);
+end

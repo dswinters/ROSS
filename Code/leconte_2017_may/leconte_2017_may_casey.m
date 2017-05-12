f464899e142b46757d79a9c5246f1f76a6a9f5eb
Dylan Winters <dylan@dw-mbp.local>
2017-05-12 13:42:59 -0800

    -

--- Code/leconte_2017_may/leconte_2017_may_casey.m
+++ Code/leconte_2017_may/leconte_2017_may_casey.m
@@ -4,11 +4,8 @@ function casey = leconte_2017_may_casey()
 %    x      |--> STARBOARD
 %  2   3   
 casey0.heading_offset = 135;
-%--------------------------------------------------------%
-dep = 1;
-casey(dep).name       = 'CASEY_test1';
-casey(dep).tlim       = datenum([;])
-casey(dep).files.adcp = {};
-casey(dep).files.gps  = {};
+casey0.tlim = [-inf inf];
+dep = 0;
+casey = struct;
 %--------------------------------------------------------%
 casey = ross_fill_defaults(casey,casey0);

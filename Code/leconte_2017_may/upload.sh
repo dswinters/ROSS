e9987b2d07bc32d18c88ab827bceaccf78a033a8
Dylan Winters <dylan@dw-mbp.local>
2017-05-12 13:44:12 -0800

    remove

--- Code/leconte_2017_may/upload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp -v ~/OSU/ROSS/Data/leconte_2017_may/Swankie/processed/*.mat /Volumes/data/20170507_Alaska/data/processed/ROSS_ADCP/.
-cp -v ~/OSU/ROSS/Data/leconte_2017_may/Rosie/processed/*.mat /Volumes/data/20170507_Alaska/data/processed/ROSS_ADCP/.
-cp -rv ~/OSU/ROSS/Figures/leconte_2017_may/Swankie/* /Volumes/data/20170507_Alaska/figures/ROSS/Swankie/.
-cp -rv ~/OSU/ROSS/Figures/leconte_2017_may/Rosie/* /Volumes/data/20170507_Alaska/figures/ROSS/Rosie/.
-

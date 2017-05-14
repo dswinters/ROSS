cb28f779c1fd91ffb04bd8d939cd94a011d643b3
Dylan Winters <dylan@dw-mbp.local>
2017-05-14 04:58:49 -0800

    change defaults

--- Code/leconte_2017_may/leconte_2017_may_deployments.m
+++ Code/leconte_2017_may/leconte_2017_may_deployments.m
@@ -1,16 +1,11 @@
-function [master deployments] = leconte_2017_may_deployments()
+function [trip deployments] = leconte_2017_may_deployments()
 
 %========================================================
 % Trip info
 %========================================================
-master                          = struct();
-master.name                     = 'leconte_2017_may';
-master.kayaks                   = {'Rosie','Swankie'};
-master.process_data             = true;
-master.make_figures.summary     = false;
-master.make_figures.surface_vel = false;
-master.make_figures.echo_intens = false;
-master.make_figures.corr        = false;
+trip = struct();
+trip.name   = 'leconte_2017_may';
+trip.kayaks = {'Rosie','Swankie'};
 
 %========================================================
 % Define some filters
@@ -21,34 +16,27 @@ filt_rotmax3   = newfilt('rotmax',3);
 notrim = newfilt('none',[]);
 
 %========================================================
-% Processing defaults
+% Processing options
 %========================================================
-defaults.proc.skip             = false;
-defaults.proc.trim_methods(1)  = trim_ei_edge_b;
-% defaults.proc.filters(1)       = filt_rotmax3;
-defaults.proc.ship_vel_removal = 'GPS';
-defaults.files.map             = 'none';
-defaults.plot.ylim             = [0 200];
-defaults.proc.adcp_raw2mat     = false;
-defaults.proc.gps_raw2mat      = false;
+opts.proc.skip             = false;
+opts.proc.trim_methods(1)  = trim_ei_edge_b;
+% opts.proc.filters(1)       = filt_rotmax3;
+opts.plot.ylim             = [0 200];
+opts.proc.adcp_raw2mat     = false;
+opts.proc.gps_raw2mat      = false;
+opts.plot.map.coastline    = '../Maps/leconte2_grid_coastline.mat';
 
 %=======================================================
 % Rosie deployments (150 hHz PAVS, Alaska flag)
 %========================================================
 rosie = leconte_2017_may_rosie();
-rosie = ross_fill_defaults(rosie,defaults);
-
-%========================================================
-% Casey deployments (300 kHz Workhorse, Norway flag)
-%========================================================
-casey = leconte_2017_may_casey();
-casey = ross_fill_defaults(casey,defaults);
+rosie = ross_fill_defaults(rosie,opts);
 
 %========================================================
 % Swankie deployments (300 kHz Sentinel V, Sweden flag)
 %========================================================
 swankie = leconte_2017_may_swankie();
-swankie = ross_fill_defaults(swankie,defaults);
+swankie = ross_fill_defaults(swankie,opts);
 
 %========================================================
 % Combine deployment structures into final cell array

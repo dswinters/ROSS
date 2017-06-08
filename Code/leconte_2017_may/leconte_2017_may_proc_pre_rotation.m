debf70ff5576f3c3eaeda1ecff7a3c44f5885b5e
Dylan Winters <dylan@dw-mbp.local>
2017-06-08 11:56:52 -0700

    move timestamp correction from pre-rot to post-load func

--- Code/leconte_2017_may/leconte_2017_may_proc_pre_rotation.m
+++ Code/leconte_2017_may/leconte_2017_may_proc_pre_rotation.m
@@ -1,12 +1,3 @@
 function [ross adcp] = leconte_2017_may_proc_pre_rotation(ross,ndep,adcp)
 
-%% Fix timestamps
-% Timestamps logged to the ROSS computers for each ADCP data ensemble appear to
-% lag behind the true ensemble times. Use the 1st logged timestamp as the start
-% time, but use the ADCP's internal clock for time since start.
-
-for i = 1:length(adcp)
-    adcp(i).mtime = adcp(i).mtime(1) + adcp(i).mtime_raw - adcp(i).mtime_raw(1);
-end
-
 

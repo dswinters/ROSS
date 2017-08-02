070a9db8c649b989c6eca6981f03b9e30f77f439
Dylan Winters <dylan@dw-mbp.local>
2017-08-02 11:08:59 -0700

    modify ctd contamination filtering

--- Code/leconte_2017_may/leconte_2017_may_proc_post_rotation.m
+++ Code/leconte_2017_may/leconte_2017_may_proc_post_rotation.m
@@ -12,13 +12,18 @@ switch id
     % signals
     for ia = 1:length(adcp)
         [N B M] = size(adcp(ia).vel);
+        u = abs(squeeze(adcp(ia).vel(:,1,:)));
+        v = abs(squeeze(adcp(ia).vel(:,2,:)));
         w = abs(squeeze(adcp(ia).vel(:,3,:)));
+        us = smooth2a(u,adcp(ia).config.n_cells,1);
+        vs = smooth2a(v,adcp(ia).config.n_cells,1);
         ws = smooth2a(w,adcp(ia).config.n_cells,1);
         mask = ones(N,M);
-        mask((w./ws)>2.5) = NaN;
+        % mask((w./ws)>2.5) = NaN;
+        mask((w./ws)>2.5 | (u./us)>2.5 | (v./vs)>2.5) = NaN;
         ms = smooth2a(mask,1,1);
         mask(ms<(3/9)) = 1;
-        for ib = 1:5
+        for ib = 1:size(adcp(1).vel,2)
             vb = squeeze(adcp(ia).vel(:,ib,:));
             vb = vb.*mask;
             adcp(ia).vel(:,ib,:) = vb;

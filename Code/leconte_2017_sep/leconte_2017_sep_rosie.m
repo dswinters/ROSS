2eb1fcbaa2a1ae8680cc82d22f73595347ed2fb0
Dylan Winters <dylan@dw-mbp.local>
2017-09-11 12:22:54 -0700

    update kayak deployment template files

--- Code/leconte_2017_sep/leconte_2017_sep_rosie.m
+++ Code/leconte_2017_sep/leconte_2017_sep_rosie.m
@@ -1,23 +1,25 @@
 function rosie = leconte_2017_sep_rosie()
+%--------------------------------------------------------%
+% ADCP orientation                                       %
+%--------------------------------------------------------%
 %        FORWARD
 %  4   1    ^
 %    x      |--> STARBOARD
 %  2   3  
-rosie0.proc.heading_offset = 135;
-rosie0.tlim = [-inf inf];
-rosie0.plot.ylim = [0 200];
-rosie0.proc.adcp_load_function = 'adcp_parse';
-rosie0.proc.ross_timestamps = 'pre';
-rosie0.proc.skip = true;
 dep=0;
 
+%--------------------------------------------------------%
+% Default deployment options                             %
+%--------------------------------------------------------%
+rosie0.proc.heading_offset = 135;
+rosie0.proc.adcp_load_function = 'adcp_parse';
+rosie0.proc.ross_timestamps = 'pre';
+swankie0.files.map = 'leconte_terminus';
+rosie0.tlim = [-inf inf];
+rosie0.plot.ylim = [0 200];
+rosie0.proc.skip = false;
+rosie0.proc.use_3beam = false;
+
+% Deployments here
 
 rosie = ross_fill_defaults(rosie,rosie0);
-
-
-dep = dep+1;
-kayak(dep).proc.skip = false;
-kayak(dep).name = 'deployment_201709time';
-kayak(dep).files.adcp = {'*_timestamped.bin'};
-kayak(dep).files.gps = {'*.log'};
-kayak(dep).plot.ylim = [0 200];
--- Code/leconte_2017_sep/leconte_2017_sep_swankie.m
+++ Code/leconte_2017_sep/leconte_2017_sep_swankie.m
@@ -13,20 +13,13 @@ dep = 0;
 %--------------------------------------------------------%
 swankie0.proc.heading_offset = 45;
 swankie0.proc.adcp_load_function = 'adcp_parse';
-swankie0.tlim = [-inf inf];
 swankie0.proc.ross_timestamps = 'pre';
-swankie0.files.map = 'none';
+swankie0.files.map = 'leconte_terminus';
+swankie0.tlim = [-inf inf];
+rosie0.plot.ylim = [0 200];
 swankie0.proc.skip = false;
 swankie0.proc.use_3beam = false;
 
-dep = dep+1;
-swankie(dep).proc.skip = false;
-swankie(dep).dir_raw = 'deployment_test';
-swankie(dep).name = 'TEST';
-swankie(dep).files.adcp = 'ADCP_timestamped*.bin';
-swankie(dep).files.gps = '*.log';
-swankie(dep).plot.ylim = [0 200];
-swankie(dep).proc.adcp_raw2mat = false;
-
+% Deployments here
 
 swankie = ross_fill_defaults(swankie,swankie0);

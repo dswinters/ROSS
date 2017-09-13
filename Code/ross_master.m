2e57b86501bdde51b67d78fc75b47d7d0fa9b137
Dylan Winters <dylan@dw-mbp.local>
2017-09-13 03:32:14 -0700

    formatting

--- Code/ross_master.m
+++ Code/ross_master.m
@@ -8,6 +8,7 @@ fn = [tripname '_deployments'];
 [trip, deployments] = feval(fn);
 
 %% Processing setup
-Ross = ross_setup(trip,deployments);         % filepaths and directories
-Ross = ross_post_setup(Ross,trip);           % trip-specific additional setup
-Ross = ross_proc_all_deployments(Ross);      % Process deployments!
+Ross = ross_setup(trip,deployments);    % filepaths and directories
+Ross = ross_post_setup(Ross,trip);      % trip-specific additional setup
+Ross = ross_proc_all_deployments(Ross); % Process deployments!
+

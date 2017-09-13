e55bf98dfa735fbdfc031acc751d54b4f6d36eb1
Dylan Winters <dylan@dw-mbp.local>
2017-09-13 01:37:52 -0700

    comments

--- Code/leconte_2017_sep/leconte_2017_sep_swankie.m
+++ Code/leconte_2017_sep/leconte_2017_sep_swankie.m
@@ -16,16 +16,16 @@ defaults.proc.heading_offset = 45;
 defaults.proc.adcp_load_function = 'adcp_parse';
 defaults.proc.ross_timestamps = 'pre';
 defaults.files.map = 'leconte_terminus';
-defaults.tlim = [-inf inf];
 defaults.plot.ylim = [0 200];
 defaults.plot.make_figure.coastline_map = true;
 defaults.proc.skip = false;
 defaults.proc.use_3beam = false;
 defaults.proc.adcp_raw2mat = true;
 
-% Deployments here
 %--------------------------------------------------------%
+% Testing on the deck - no useable data!
 dep = dep+1;
+swankie(dep).proc.skip = true;
 swankie(dep).name          = 'swankie_decktest_20170912';
 swankie(dep).dirs.raw_gps  = 'decktest_20170912/GPS/';
 swankie(dep).dirs.raw_adcp = 'decktest_20170912/ADCP/';

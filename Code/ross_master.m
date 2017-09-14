05c841675ecbef87a23323a01dbcd4fa414279ee
Dylan Winters <dylan@dw-mbp.local>
2017-09-13 22:36:27 -0800

    add missing semicolon

--- Code/ross_master.m
+++ Code/ross_master.m
@@ -3,7 +3,7 @@ function ross_master(cruise_name)
 addpath(cruise_name); % add cruise-specific functions to path
 addpath('figures/');  % add figure functions to path
 
-[cruise,deployments] = cruise_config(cruise_name) % Cruise configuration
-Config = ross_setup(cruise,deployments);          % filepaths and directories
-Config = ross_proc_all_deployments(Config);       % Process deployments!
+[cruise,deployments] = cruise_config(cruise_name); % Cruise configuration
+Config = ross_setup(cruise,deployments);           % filepaths and directories
+Config = ross_proc_all_deployments(Config);        % Process deployments!
 

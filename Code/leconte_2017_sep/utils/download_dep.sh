e7b7d3af10a991285ffc501709fcdd7c9fd080db
Dylan Winters <dylan@dw-mbp.local>
2017-09-13 17:16:46 -0700

    fix download script

--- Code/leconte_2017_sep/utils/download_dep.sh
+++ Code/leconte_2017_sep/utils/download_dep.sh
@@ -6,5 +6,5 @@ base="${HOME}/OSU/ROSS"
 rdep="${SCISHARE}/data/raw/ROSS/${kayak}/deployment_201709${dep}/"
 ldep="${base}/Data/${TRIP}/${kayak}/raw/deployment_201709${dep}/"
 
-echo cp -rv $rdep $ldep
+cp -rv $rdep $ldep
 

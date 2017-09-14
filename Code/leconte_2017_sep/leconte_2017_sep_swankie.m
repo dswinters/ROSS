d1ab6a36b3cf0cc0f38973e8084aad5371fd62ba
Dylan Winters <dylan@dw-mbp.local>
2017-09-13 17:17:21 -0700

    add first swankie deployment

--- Code/leconte_2017_sep/leconte_2017_sep_swankie.m
+++ Code/leconte_2017_sep/leconte_2017_sep_swankie.m
@@ -21,17 +21,32 @@ defaults.plot.make_figure.coastline_map = true;
 defaults.proc.skip = false;
 defaults.proc.use_3beam = false;
 defaults.proc.adcp_raw2mat = true;
+trim_corr_edge = struct('name','corr_edge','params','beam');
+defaults.proc.trim_methods(1) = trim_corr_edge;
 
 %--------------------------------------------------------%
 % Testing on the deck - no useable data!
-dep = dep+1;
+% dep = dep+1;
 % swankie(dep).proc.skip = true;
-swankie(dep).name          = 'swankie_decktest_20170912';
-swankie(dep).dirs.raw_gps  = 'decktest_20170912/GPS/';
-swankie(dep).dirs.raw_adcp = 'decktest_20170912/ADCP/';
+% swankie(dep).name          = 'swankie_decktest_20170912';
+% swankie(dep).dirs.raw_gps  = 'decktest_20170912/GPS/';
+% swankie(dep).dirs.raw_adcp = 'decktest_20170912/ADCP/';
+% swankie(dep).files.adcp    = '*timestamped*.bin';
+% swankie(dep).files.gps     = '*.log';
+% swankie(dep).plot.ylim     = [0 200];
+
+%--------------------------------------------------------%
+dep = dep+1;
+swankie(dep).name          = 'swankie_deployment_20170913132345';
+swankie(dep).dirs.raw_gps  = 'deployment_20170913132345/GPS/';
+swankie(dep).dirs.raw_adcp = 'deployment_20170913132345/ADCP/';
 swankie(dep).files.adcp    = '*timestamped*.bin';
 swankie(dep).files.gps     = '*.log';
 swankie(dep).plot.ylim     = [0 200];
+swankie(dep).tlim = datenum([...
+    '13-Sep-2017 14:03:47';
+    '13-Sep-2017 17:01:19']);
+
 
 % Fill defaults
 if ~isempty(swankie)

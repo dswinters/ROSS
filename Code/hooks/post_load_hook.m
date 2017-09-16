e2f7128ba8fa5afd3f1af82e90d2f5afa8223241
Dylan Winters <dylan@dw-mbp.local>
2017-09-16 13:48:23 -0800

    fix typo

--- Code/hooks/post_load_hook.m
+++ Code/hooks/post_load_hook.m
@@ -1,6 +1,6 @@
 function [config, adcp, gps] = post_load_hook(config,adcp,gps)
 
-func = [config.cruse '_post_load_hook'];
+func = [config.cruise '_post_load_hook'];
 if exist(fn) == 2
     [config, adcp, gps] = feval(func,config,adcp,gps);
 end
--- Code/hooks/pre_rotation_hook.m
+++ Code/hooks/pre_rotation_hook.m
@@ -1,6 +1,6 @@
 function [config, adcp, gps] = pre_rotation_hook(config,adcp,gps)
 
-func = [config.cruse '_pre_rotation_hook'];
+func = [config.cruise '_pre_rotation_hook'];
 if exist(fn) == 2
     [config, adcp] = feval(func,config,adcp);
 end

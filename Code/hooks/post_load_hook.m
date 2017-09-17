57e223f56ed620232cc1d09ecdcd89edfe663f08
Dylan Winters <dylan@dw-mbp.local>
2017-09-16 21:39:06 -0800

    fix post_load_hook

--- Code/hooks/post_load_hook.m
+++ Code/hooks/post_load_hook.m
@@ -2,5 +2,5 @@ function [deployment, adcp, gps] = post_load_hook(deployment, adcp, gps)
 
 func = [deployment.cruise.name '_post_load_hook'];
 if exist(func) == 2
-    [config, adcp, gps] = feval(func,config,adcp,gps);
+    [config, adcp, gps] = feval(func,deployment,adcp,gps);
 end

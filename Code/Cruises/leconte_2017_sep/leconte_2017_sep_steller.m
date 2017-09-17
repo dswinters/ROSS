ea925e42e2fde9a93e7b283bda21b284fe4c88f2
Dylan Winters <dylan@dw-mbp.local>
2017-09-17 03:25:40 -0800

    add more settings for steller/ADCP_steller

--- Code/Cruises/leconte_2017_sep/leconte_2017_sep_steller.m
+++ Code/Cruises/leconte_2017_sep/leconte_2017_sep_steller.m
@@ -9,7 +9,7 @@ function config = leconte_2017_sep_steller()
 config.name = 'Steller';
 dep = 0;
 
-defaults.proc.heading_offset = 55;
+defaults.proc.heading_offset = 31.23;
 defaults.proc.adcp_load_func = 'adcp_rdradcp_multi';
 defaults.proc.nmea = {'GPRMC','HEHDT'};
 defaults.dirs.raw = ''; % All raw data is in the same folder
@@ -18,8 +18,18 @@ dep = dep+1;
 steller(dep).name = 'ADCP_steller';
 steller(dep).files.gps = '*.N1R';
 steller(dep).files.adcp = '*.ENR';
-steller(dep).proc.adcp_raw2mat = true; % refresh ADCP mat files
-steller(dep).proc.gps_raw2mat = true; % refresh GPS mat files;
+steller(dep).proc.adcp_raw2mat = false; % refresh ADCP mat files
+steller(dep).proc.gps_raw2mat  = false; % refresh GPS mat files;
+steller(dep).plot.lonlim = [-132.5350,-132.3470];
+steller(dep).plot.latlim = [56.7164, 56.8462];
+steller(dep).plot.make_figure.summary = true;
+steller(dep).plot.make_figure.surface_vel = false;
+steller(dep).plot.make_figure.echo_intens = false;
+steller(dep).plot.make_figure.corr = false;
+steller(dep).plot.make_figure.coastline_map = true;
+
+%* Diagnostics
+% steller(dep).proc.diagnostic = true;
 
 steller = fill_defaults(steller,defaults);
 config.deployments = steller;

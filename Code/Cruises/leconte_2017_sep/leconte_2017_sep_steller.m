bf09fd51d6b9bf23d49caa688fc5a30d64e6a07b
Dylan Winters <dylan@dw-mbp.local>
2017-10-11 12:38:41 -0700

    save unstaged steller changes

--- Code/Cruises/leconte_2017_sep/leconte_2017_sep_steller.m
+++ Code/Cruises/leconte_2017_sep/leconte_2017_sep_steller.m
@@ -7,14 +7,15 @@ function vessel = leconte_2017_sep_steller()
 %    x      |--> STARBOARD
 %  4   2   
 vessel.name = 'Steller';
-scishare = '/Volumes/data/20170912_Alaska/';
+dbox = fullfile([getenv('DROPBOX'), '/']);
+scishare = '/Volumes/Norgannon/ScienceShare/20170912_Alaska/';
 vessel.dirs.raw  = [scishare 'data/raw/ADCP_steller/'];
-vessel.dirs.proc = [scishare 'data/processed/ADCP_steller/'];
-vessel.dirs.figs = [scishare 'figures/ADCP_steller/'];
+vessel.dirs.proc = [dbox 'LeConte/Data/ocean/september2017/processed/ADCP_steller/'];
+vessel.dirs.figs = [dbox 'LeConte/Data/ocean/september2017/processed/ADCP_steller/figures/'];
 
 dep = 0;
 
-defaults.proc.heading_offset = 40.86 - 8.5; % adcp mounting offset + hemisphere mounting offset
+defaults.proc.heading_offset = 40.86 - 9.22; % adcp mounting offset + hemisphere mounting offset
 defaults.proc.adcp_load_func = 'adcp_rdradcp_multi';
 defaults.proc.nmea = {'GPRMC','HEHDT'};
 defaults.dirs.raw = ''; % All raw data is in the same folder
@@ -23,8 +24,8 @@ dep = dep+1;
 deployment(dep).name = 'ADCP_steller';
 deployment(dep).files.gps = '*.N1R';
 deployment(dep).files.adcp = '*.ENR';
-% deployment(dep).proc.adcp_raw2mat = true; % refresh ADCP mat files
-% deployment(dep).proc.gps_raw2mat  = true; % refresh GPS mat files;
+deployment(dep).proc.adcp_raw2mat = true; % refresh ADCP mat files
+deployment(dep).proc.gps_raw2mat  = true; % refresh GPS mat files;
 deployment(dep).plot.lonlim = [-132.5350,-132.3470];
 deployment(dep).plot.latlim = [56.7164, 56.8462];
 deployment(dep).plot.make_figure.summary = true;
@@ -51,4 +52,3 @@ deployment(dep).plot.make_figure.coastline_map = false;
 
 deployment = fill_defaults(deployment,defaults);
 vessel.deployment = deployment;
-
--- Code/cruises/leconte_2017_sep/leconte_2017_sep_post_rotation_hook.m
+++ Code/cruises/leconte_2017_sep/leconte_2017_sep_post_rotation_hook.m
@@ -1,5 +1,10 @@
 function [DEP, adcp] = leconte_2017_sep_post_rotation_hook(DEP,adcp)
 
+return
+
+
+
+
 switch DEP.vessel.name
   case 'Steller'
     disp('Correcting Steller "wag"...')
@@ -7,9 +12,7 @@ switch DEP.vessel.name
     % Since the external heading source is mounted at an offset from the ADCP,
     % rotations about the heading source translate to lateral sensor motion.
 
-    % wag_coeff = 0; % no correction
-    % wag_coeff = 0.0634; % (m/s)/(deg/s), measured
-    wag_coeff = 0.21; % (m/s)/(deg/s), estimated
+    d = -0.9 - 12.6i; % ADCP/Hemisphere separation
 
     % Load GPS data to calculate rate of turn
     gps = load_gps(DEP);
@@ -20,17 +23,24 @@ switch DEP.vessel.name
     rot(idx) = interp1(gps.dn(~idx),rot(~idx),gps.dn(idx));
     rot = interp1(gps.dn,rot,adcp.mtime);
 
-    % Rotate to ship coords by removing heading
+    % Caclulate rotation-induced translation at ADCP
+    %               heading  *     components   *   radial velocity
+    rotvel = exp(1i*deg2rad(90-h)) * d/abs(d) * 2*pi*abs(d) * rot/360;
     v = squeeze(adcp.vel(:,1,:)) + 1i*squeeze(adcp.vel(:,2,:));
-    v = v .*  repmat(exp(1i*deg2rad(adcp.heading + 8.5)),adcp.config.n_cells,1);
+    v = v + repmat(rotvel,adcp.config.n_cells,1);
+
+    % % Rotate to ship coords by removing heading
+    % v = v .*  repmat(exp(1i*deg2rad(adcp.heading + 8.5)),adcp.config.n_cells,1);
     
-    % Remove wag
-    v = v + repmat(rot*wag_coeff,adcp.config.n_cells,1);
+    % % Remove wag
+    % v = v + repmat(rot*wag_coeff,adcp.config.n_cells,1);
     
-    % Rotate back to earth coords by adding heading
-    v = v .*  repmat(exp(-1i*deg2rad(adcp.heading + 8.5)),adcp.config.n_cells,1);
+    % % Rotate back to earth coords by adding heading
+    % v = v .*  repmat(exp(-1i*deg2rad(adcp.heading + 8.5)),adcp.config.n_cells,1);
 
     % Store new velocities in ADCP struct
+
     adcp.vel(:,1,:) = real(v);
     adcp.vel(:,2,:) = imag(v);
 end
+

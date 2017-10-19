827faec3ff60a494bdba5ad54e83e6cc470f2c90
Dylan Winters <dylan@dw-mbp.local>
2017-10-19 11:55:48 -0700

    don't use compass calibration for now

--- Code/cruises/asiri_2015_aug/asiri_2015_aug_pre_rotation_hook.m
+++ Code/cruises/asiri_2015_aug/asiri_2015_aug_pre_rotation_hook.m
@@ -1,27 +1,24 @@
 function [dep, adcp] = asiri_2015_aug_pre_rotation_hook(dep,adcp)
-% This function is called before rotation ADCP velocities to earth corodinates.
-% Since we don't have any external heading data, we transform the raw heading
-% data with a fit:
 
-% Fit info
-comps =@(h) [ones(length(h),1) ,...
-             sind(h(:))        ,...
-             cosd(h(:))        ,...
-             sind(2*h(:))      ,...
-             cosd(2*h(:))];
-fitfmt = '%.1f + %.1fsin(c) + %.1fcos(c) + %.1fsin(2c) + %.1fcos(2c)';
-coeffs = [201.8
-          31.5
-          13.3
-          -4.4
-          1.0];
-fitstr = sprintf(fitfmt,coeffs);
+%% Apply compass calibration for true heading
+% comps =@(h) [ones(length(h),1) ,...
+%              sind(h(:))        ,...
+%              cosd(h(:))        ,...
+%              sind(2*h(:))      ,...
+%              cosd(2*h(:))];
+% fitfmt = 'c + %.1f + %.1fsin(c) + %.1fcos(c) + %.1fsin(2c) + %.1fcos(2c)';
+% coeffs = [161.7383; -31.7958; -16.7760; 1.3896; 2.9322];
+% fitstr = sprintf(fitfmt,coeffs);
 
-adcp.heading = mod(adcp.heading + [comps(adcp.heading)*coeffs]', 360);
-adcp.gps.heading = adcp.heading;
+% adcp.heading = mod(adcp.heading_internal + [comps(adcp.heading_internal)*coeffs]', 360);
+% % adcp.gps.h = adcp.heading;
 
-info = {'Heading obtained by calibrating internal ADCP compass to GPS track heading';
-        'Heading calibration speed threshold: > 2.5 m/s';
-        ['Compass -> heading equation: h = ' fitstr]};
-adcp.info = cat(1,adcp.info,info);
+% info = {'Heading obtained by calibrating internal ADCP compass to GPS track heading';
+%         'Heading calibration speed threshold: > 2.5 m/s';
+%         ['Compass -> heading equation: h = ' fitstr]};
+% adcp.info = cat(1,adcp.info,info);
 
+
+%% Remove data with poor correlation
+adcp.vel(adcp.corr<40) = NaN;
+adcp.info = cat(1,adcp.info,{'Beam data removed where correlation magnitude < 40'});

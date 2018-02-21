c5b977d8c4ba2349db1fa98daabf4e37d60832bc
Dylan Winters <dylan@dw-mbp.local>
2018-02-21 15:27:38 -0800

    update to newport 2018 jan config

--- Code/cruises/newport_2018_jan/config_newport_2018_jan.m
+++ Code/cruises/newport_2018_jan/config_newport_2018_jan.m
@@ -6,16 +6,19 @@ vessel.dirs.raw = fullfile(getenv('HOME'),'Downloads/Data/20180131-Newport/');
 vessel.dirs.proc = fullfile(getenv('HOME'),'Downloads/Data/20180131-Newport/Processed/');
 vessel.dirs.figs = fullfile(getenv('HOME'),'Downloads/Data/20180131-Newport/Figures/');
 
+trim_ei_edge = struct('name','ei_edge','params','beam');
 defaults.proc.heading_offset      = 45;
 defaults.proc.adcp_rotation_func  = 'adcp_beam2earth';
-defaults.proc.filters(1)          = struct('name','corrmin','params',255);
+% defaults.proc.filters(1)          = struct('name','corrmin','params',0);
 defaults.proc.adcp_raw2mat        = true;
 defaults.proc.gps_raw2mat         = true;
-defaults.proc.adcp_load_func      = 'adcp_parse';
-defaults.proc.adcp_load_args      = {'ross','pre'};
-defaults.proc.nmea                = {'GPRMC','HEHDT','PASHR','GPGGA'};
+defaults.proc.adcp_load_func      = 'adcp_rdradcp_multi';
+defaults.proc.trim_methods(1)     = trim_ei_edge;
+% defaults.proc.adcp_load_args      = {'ross','pre'};
+defaults.proc.nmea                = {'GPRMC','HEHDT','GPGGA'};
 defaults.files.gps                = 'GPS/*.log';
 defaults.files.adcp               = 'ADCP/*timestamped*.bin';
+defaults.files.adcp               = 'ADCP/*raw*.bin';
 defaults.plot.make_figure.summary = true;
 
 dep = struct();

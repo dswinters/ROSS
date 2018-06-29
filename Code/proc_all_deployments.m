ad18bd5917d468f925bb4267cb3dc66ee719804f
Dylan Winters <dylan.s.winters@gmail.com>
2018-06-29 09:50:11 -0700

    more detailed error reporting

--- Code/proc_all_deployments.m
+++ Code/proc_all_deployments.m
@@ -8,7 +8,11 @@ for nves = 1:length(Vessels)
             Vessels(nves).deployment(ndep) = ...
                 proc_deployment(Vessels(nves).deployment(ndep));
         catch err
-            disp(sprintf('Skipped %s',Vessels(nves).deployment(ndep).name))
+            fprintf('  Error: %s\n',err.message)
+            for i = 1:length(err.stack)
+                fprintf('    %s (%d)\n',err.stack(i).name,err.stack(i).line)
+            end
+            disp(sprintf('  Skipped %s',Vessels(nves).deployment(ndep).name))
         end
     end
 end
